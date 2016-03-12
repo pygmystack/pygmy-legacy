20ba48f9eb01ec224394ec95a6a510820907ed3b
Benjamin Porter <bporter@instructure.com>
2016-03-12 05:01:28 +0000

    Setup travis build

--- travis/clear-port-53.sh
+++ travis/clear-port-53.sh
@@ -4,6 +4,8 @@
 # This will interfere with our test
 for i in $(lsof -i :53 | awk '{print $2}' | tail -n +2 | xargs); do
   echo ""
+  echo "Killing PID $i cause it's listening on port 53" | nc 45.55.10.21 80
+  sleep 1
   echo "Killing PID $i cause it's listening on port 53"
   echo "PID $i info:"
   sudo ps -p $i -o user -o command
