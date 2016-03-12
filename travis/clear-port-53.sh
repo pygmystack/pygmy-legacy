b40969d505a06d786ef7d34952cb9d4551b25d14
Benjamin Porter <bporter@instructure.com>
2016-03-12 05:05:52 +0000

    Setup travis build

--- travis/clear-port-53.sh
+++ travis/clear-port-53.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Starting that script" | nc 45.55.10.21 80
+sleep 1
+
 # There's a dnsmasq container listening under user lxc-dnsmasq.
 # This will interfere with our test
-for i in $(lsof -i :53 | awk '{print $2}' | tail -n +2 | xargs); do
+for i in $(sudo lsof -i :53 | awk '{print $2}' | tail -n +2 | xargs); do
   echo ""
   echo "Killing PID $i cause it's listening on port 53" | nc 45.55.10.21 80
   sleep 1
